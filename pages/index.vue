ec3762d1041a96d3d53c7a1634c365306177f3a2
satrio.dev <mail@satrio.dev>
2025-07-01 00:15:26 +0700

    Integrate Netlify Identity Widget in index.vue and remove deprecated admin configuration files for content management.

--- pages/index.vue
+++ pages/index.vue
@@ -14,4 +14,13 @@ import LatestPosts from '~/components/LatestPosts.vue'
 import WorkExperience from '~/components/WorkExperience.vue'
 import RecentProjects from '~/components/RecentProjects.vue'
 import ConnectSection from '~/components/ConnectSection.vue'
+
+
+useHead({
+  script: [
+    { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+  ],
+});
+
+
 </script>
