bf3148bc6b4c4359ca816f09b6468fccc22325c1
Satriyo Unggul Wicaksono <stryuw@gmail.com>
2025-11-26 20:15:46 +0700

    Fix typos and formatting in HeroSection.vue

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -3,14 +3,14 @@
         <p class="font-bold">Hi, I'm Satriyo 👋🏻</p>
         <p>Full-Stack Engineer with over three years of experience at Indonesia's national meteorological agency (BMKG) and currently serving as the Lead Software Developer for the Open Knowledge Association (OKA).</p>
         <p>My core competency is building scalable, high-performance web
-applications using a diverse technology stack, including Python ,
-PHP , Node.js , and Ruby on Rails. I have a strong passion for
+applications using a diverse technology stack, including Python,
+PHP, Node.js, and Ruby on Rails. I have a strong passion for
 bridging Earth Science data (Meteorology) with efficient and elegant
 software solutions.</p>
         <p>In addition to my backend roles, I am experienced in modern
-frontends (JavaScript , React) and dedicated to UX principles.</p>
+frontends (JavaScript, React) and dedicated to UX principles.</p>
         <p>
-            I am passionate about technology, innovation , and open-source
+            I am passionate about technology, innovation, and open-source
 contribution. My goal is to lead development and deliver web
 solutions that enhance the user experience and provide value to
 stakeholders.
